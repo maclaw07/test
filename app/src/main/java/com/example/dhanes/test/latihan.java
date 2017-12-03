74adeb7c1adc694ab7c3c99585700cbb948f3761
Dhanes <nes07197@gmail.com>
2017-12-03 10:10:39 +0800

    initial commit

---
+++ app/src/main/java/com/example/dhanes/test/latihan.java
@@ -0,0 +1,13 @@
+package com.example.dhanes.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class latihan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_latihan);
+    }
+}
